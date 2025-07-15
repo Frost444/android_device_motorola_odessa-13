71d91507f0115edbb813bfc0f24037685389fbc1
Frost444 <thiagojurema20@gmail.com>
2025-07-15 12:08:49 -0300

    derp

--- derp_odessa.mk
+++ derp_odessa.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/odessa/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Flags
 TARGET_DISABLE_EPPE := true
