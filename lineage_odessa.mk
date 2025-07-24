70dc8558ec4e240288f61a98c82ad7e5d013908d
Frost444 <thiagojurema20@gmail.com>
2025-07-24 14:04:33 -0300

    sm6150-common: Disable QS media player turbulence effect by default
    - This turbulence effect causes tremendous lags on devices with a midrange/low-end GPU as it requires expensive rendering.
    - Hence, disable it by default to alleviate the situation.

--- lineage_odessa.mk
+++ lineage_odessa.mk
@@ -21,8 +21,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from odessa device
 $(call inherit-product, device/motorola/odessa/device.mk)
 
-TARGET_SUPPORTS_OMX_SERVICE := false
-
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
