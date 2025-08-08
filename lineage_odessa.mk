50a02d9b6d9da37e51c1af7722caf9b5f00a8f23
Frost444 <thiagojurema20@gmail.com>
2025-08-08 13:57:49 -0300

    bringup

--- lineage_odessa.mk
+++ lineage_odessa.mk
@@ -29,7 +29,7 @@ TARGET_DISABLE_EPPE := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Signing Build
-#-include vendor/lineage-priv/keys/keys.mk
+-include vendor/evolution-priv/keys/keys.mk
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_odessa
