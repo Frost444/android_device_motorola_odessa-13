8611d56092e2d8e9e2f27edfef406e6cfb689977
Frost444 <thiagojurema20@gmail.com>
2026-01-14 18:31:22 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_odessa.mk
+    $(LOCAL_DIR)/aosp_odessa.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_odessa-eng \
-    derp_odessa-user \
-    derp_odessa-userdebug
+    aosp_odessa-eng \
+    aosp_odessa-user \
+    aosp_odessa-userdebug
